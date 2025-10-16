e82055e638fb8fe5122b0311772e2e77c3258b88
Joy Swarnakar <123759014+JOYSWARNAKAR@users.noreply.github.com>
2025-10-16 04:52:56 +0000

    Number, boolean and type inference

---
+++ 01_intro/intro.ts
@@ -0,0 +1,4 @@
+let user = {name: "joy", age: 20, password:  235}
+console.log(user.name);
+// tsc intro.ts -> create a separeate .js file because
+// all ts code transform js code
---
+++ test.ts
@@ -0,0 +1,7 @@
+console.log("Helllo");
+
+let number1 = "3"
+let number2 = 3
+let sum = number1 + number2
+console.log(sum);
+
